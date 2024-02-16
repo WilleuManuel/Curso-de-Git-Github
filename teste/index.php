9998ee9e1f95d755d1434fb5ee32b1a45ef468b0
Willeu Manuel <willeumanuel@gmail.com>
2024-02-16 14:56:38 +0100

    Modifiquei o arquivo index.php que está pasta teste

---
+++ teste/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="author" content="António Domingos">
+	<title>Curso de Git e Github</title>
+</head>
+<body>
+
+	<p>Olá mundo!</p>
+	<span>Este é um curso de Git & Github</span>
+	<em>António Domingos</em> | <strong><?= (new DateTime)->format('Y') ?></strong>
+
+</body>
+</html>
